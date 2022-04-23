5c94bd00311d86601592f284c6f5b2c554f43277
Żegota Marta <marta.zegota@pgn.corp>
2022-04-23 14:01:50 +0200

    Remove part code

--- lista.py
+++ lista.py
@@ -6,8 +6,3 @@ shopping_dict = {
 shop_dict = dict(shopping_dict)
 for shop, product in shop_dict.items():
   print(f"Idę do {shop} i kupuję następujące {product}")
-
-sum_product = shopping_dict.get("piekarnia") + shopping_dict.get("warzywniak")
-
-number_product = len(sum_product)
-print(f"\nW sumie kupuję {number_product} produktów.")
