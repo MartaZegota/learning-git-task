71daba7a401f7dffcc2843d0c12815003f51c19e
Żegota Marta <marta.zegota@pgn.corp>
2022-04-23 10:00:30 +0200

    Init project

--- lista.py
+++ lista.py
@@ -1,1 +1,13 @@
-print('sth')
+shopping_dict = {
+    "piekarnia": ['chleb', 'pączek', 'bułki'],
+    "warzywniak": ['marchew', 'seler', 'rukola']
+}
+
+shop_dict = dict(shopping_dict)
+for shop, product in shop_dict.items():
+  print(f"Idę do {shop} i kupuję następujące {product}")
+
+sum_product = shopping_dict.get("piekarnia") + shopping_dict.get("warzywniak")
+
+number_product = len(sum_product)
+print(f"\nW sumie kupuję {number_product} produktów.")
